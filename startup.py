a4af1a41045642588595bbc2c7ad59908eddbc51
Craig West <cawest1221@gmail.com>
2015-03-26 18:37:02 -0400

    refactored startup file

---
+++ startup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# Creation script - this creates a client process and a set of server databases (all locally) that will conduct Goldbergs PIR protocol
+# PIR - Goldbergs Protocol
+#CS 54701 Project- Craig West & Michael Kouremetis
+
+import os
+import sys
+import time
+import argparse
+
+parser = argparse.ArgumentParser(description='Launch the Client and DB for PIR')
+parser.add_argument("-n", "--n", type=int, dest='l_num_datab', nargs='?', default=2, help="the # of databases for the system")
+parser.add_argument("-c", "--collude", type=int, dest='t_priv_num_datab', nargs='?', default=0, help="the # of databases that can collude and still not obtain query, t+1 servers must collude to break IT-PIR")
+parser.add_argument("-r", "--respond", type=int, dest='k_req_num_datab', nargs='?', default=0, help="the # of databases that need to respond to query and query still be successfully retrieved")
+parser.add_argument("-m", "--malicious", type=int, dest='v_byz_num_datab', nargs='?', default=0, help="the # of databases ( v of k ) that can be malicious, i.e. lie or be erroneous for query to still be successful")
+parser.add_argument("-q", "--query", dest='query_set_fn', nargs='?', default='PIR-Queries.csv', help="filename of set of queries to make the servers; format of file is probably just indexes  by csv , 10 per row")
+parser.add_argument("-d", "--database", dest='datab_config', nargs='?', default='PIR-Databases.csv', help="filename of set of databases addresses to use; format is (host, port, db type) , one per line")
+args = parser.parse_args()
+
+
+# Set up databases (servers) first
+#this opens up a terminal for every server started so can observe it
+for index in range(0, args.l_num_datab):
+    os.system('''gnome-terminal -e 'bash -c "python PIR-Goldberg-DB.py {0} {1}"' '''.format(index, args.datab_config))
+
+
+time.sleep(3)
+
+#Set up client
+os.system('''gnome-terminal -e 'bash -c "python PIR-Goldberg-Client.py {0} {1} {2} {3} {4} {5}"'  '''.format(args.l_num_datab, args.t_priv_num_datab,
+    args.k_req_num_datab, args.v_byz_num_datab, args.query_set_fn, args.datab_config))
